a95a78de8ee41d07375757f21d722d8963929c74
Yury Velikanau <yury.velikanau@gmail.com>
2017-08-10 11:42:12 -0700

    Add aliases for Heroku dump/psql when database is not on heroku.

--- locals/zsh.after/aliases.zsh
+++ locals/zsh.after/aliases.zsh
@@ -1,3 +1,7 @@
 alias be="bundle exec"
 alias rbe="rbenv"
+
 alias heroku='nocorrect noglob /usr/local/bin/heroku'
+
+alias heroku:psql='_heroku_psql() { psql `heroku config:get DATABASE_URL -a $1`};_heroku_psql'
+alias heroku:dump='_heroku_dump() { pg_dump -f $1.dump -F c -v -x `heroku config:get DATABASE_URL -a $1`};_heroku_dump'
