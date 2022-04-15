26406071855d1e5395323ef5a6d97585aacf8b15
Yury Velikanau <yury.velikanau@gmail.com>
2022-04-15 12:20:19 -0700

    Few changes

--- locals/zsh.after/elixir.zsh
+++ locals/zsh.after/elixir.zsh
@@ -1,1 +1,5 @@
 export ERL_AFLAGS="-kernel shell_history enabled"
+export KERL_BUILD_DOCS="yes"
+
+path+=('/Users/spectator/.mix/escripts')
+export PATH
--- locals/zsh.after/kube.zsh
+++
@@ -1,1 +0,0 @@
-export KUBECONFIG=$KUBECONFIG:$HOME/.kube/config
